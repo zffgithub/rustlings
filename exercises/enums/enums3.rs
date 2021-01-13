f0ea73d3ed43f4b80179876ab89456088219b98d
ZhuFangfang <zffforgithub@gmail.com>
2021-01-13 18:23:20 +0800

    add ennum

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -5,6 +5,10 @@
 
 enum Message {
     // TODO: implement the message variant types based on their usage below
+    Move(Point),
+    Echo(String),
+    ChangeColor((u8, u8, u8)),
+    Quit,
 }
 
 struct Point {
@@ -37,6 +41,20 @@ impl State {
 
     fn process(&mut self, message: Message) {
         // TODO: create a match expression to process the different message variants
+        match message {
+            Message::ChangeColor(color) => {
+                self.change_color(color)
+            },
+            Message::Echo(s) => {
+                self.echo(s)
+            },
+            Message::Move(p) => {
+                self.move_position(p)
+            },
+            Message::Quit => {
+                self.quit()
+            },
+        }
     }
 }
 
